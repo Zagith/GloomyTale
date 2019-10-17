1483fa6a3465a6680e8b012fee5c7d839bab8a43
Zagith <zagith95@gmail.com>
2019-10-17 14:59:30 +0200

    start title system

---
+++ OpenNos.Mapper/Mappers/CharacterTitleMapper.cs
@@ -0,0 +1,43 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class CharacterTitleMapper
+    {
+        public static bool ToCharacterRelation(CharacterRelationDTO input, CharacterTitleDTO output)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+
+            output.CharacterId = input.CharacterId;
+            output.TitleId = input.TitleId;
+            output.IsUsed = input.IsUsed;
+            output.IsDisplay = input.IsDisplay;
+
+            return true;
+        }
+
+        public static bool ToCharacterRelationDTO(CharacterTitle input, CharacterTitleDTO output)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+
+            output.CharacterId = input.CharacterId;
+            output.TitleId = input.TitleId;
+            output.IsUsed = input.IsUsed;
+            output.IsDisplay = input.IsDisplay;
+
+            return true;
+        }
+    }
+}
