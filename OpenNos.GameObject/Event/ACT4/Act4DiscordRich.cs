dc8649d9d8be75ac35bcf1ddc176695cda28829f
Zagith <zagith95@gmail.com>
2020-01-04 13:36:04 +0100

    fix discord server status

--- OpenNos.GameObject/Event/ACT4/Act4DiscordRich.cs
+++ OpenNos.GameObject/Event/ACT4/Act4DiscordRich.cs
@@ -1,10 +1,6 @@
 ﻿using Discord;
 using Discord.Webhook;
 using OpenNos.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OpenNos.GameObject.Event.ACT4
--- OpenNos.GameObject/Helpers/DiscordHelper.cs
+++ OpenNos.GameObject/Helpers/DiscordHelper.cs
@@ -15,7 +15,7 @@ namespace OpenNos.GameObject.Helpers
             // The webhook url follows the format https://discordapp.com/api/webhooks/{id}/{token}
             // Because anyone with the webhook URL can use your webhook
             // you should NOT hard code the URL or ID + token into your application.
-            string message = "Server Online";
+            string message = "**Server Online**";
             using (var client = new DiscordWebhookClient("https://discordapp.com/api/webhooks/661214889875996672/b-fY_M3_X9EOfQWLLNs5fNmvV3rkuao7PjJz6Ni1w1Dh0pEHTe6NeJNpeshc73yoke7b"))
             {
                 // Webhooks are able to send multiple embeds per message
