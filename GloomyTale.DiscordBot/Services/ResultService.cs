ee9ff9557a50f0f3a02a00316547006bcd96b304
Zagith <zagith95@gmail.com>
2020-01-07 17:29:35 +0100

    start music discord bot

---
+++ GloomyTale.DiscordBot/Services/ResultService.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+
+namespace GloomyTale.DiscordBot.Services
+{
+    public class ResultService : RuntimeResult
+    {
+        public ResultService(CommandError? error, string reason) : base(error, reason)
+        {
+        }
+
+        public static ResultService FromError(string reason) =>
+            new ResultService(CommandError.Unsuccessful, reason);
+        public static ResultService FromStrangeError(string strangeReason) =>
+            new ResultService(CommandError.Unsuccessful, $"A strange error has occured, please send this to the support channel in our Discord server (~support).\n`{strangeReason}`");
+        public static ResultService FromSuccess(string reason = null) =>
+            new ResultService(null, reason);
+    }
+}
