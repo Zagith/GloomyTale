643a4f4c7e2bd6650d15215986c99547363d8483
Zagith <zagith95@gmail.com>
2020-01-21 23:50:26 +0100

    fix potential crashes

--- GloomyTale.DiscordBot/Modules/FrozenCrownModule.cs
+++ GloomyTale.DiscordBot/Modules/FrozenCrownModule.cs
@@ -17,23 +17,34 @@ namespace GloomyTale.DiscordBot.Modules
             if (Context.Channel.Name == "bot-commands")
             {
                 Tuple<int, int> stats = DiscordServiceClient.Instance.GetAct4Stat();
-                string message = "Act4 stats:";
-                EmbedBuilder angel = new EmbedBuilder
+                if (stats != null)
                 {
-                    Color = Color.Gold,
-                    ImageUrl = "https://cdn.discordapp.com/attachments/420774865369300993/650332113576525844/angel.png",
-                    Title = "Faction Angel",
-                    Description = $"Percentage: {stats.Item1}%"
-                };
-                await ReplyAsync(message, false, angel.Build());
-                EmbedBuilder demon = new EmbedBuilder
-                {
-                    ImageUrl = "https://cdn.discordapp.com/attachments/420774865369300993/650332109025574935/demon.png",
-                    Title = "Faction Demon",
-                    Description = $"Percentage: {stats.Item2}%"
-                };
+                    string message = "Act4 stats:";
+                    EmbedBuilder angel = new EmbedBuilder
+                    {
+                        Color = Color.Gold,
+                        ImageUrl = "https://cdn.discordapp.com/attachments/420774865369300993/650332113576525844/angel.png",
+                        Title = "Faction Angel",
+                        Description = $"Percentage: {stats.Item1}%"
+                    };
+                    await ReplyAsync(message, false, angel.Build());
+                    EmbedBuilder demon = new EmbedBuilder
+                    {
+                        ImageUrl = "https://cdn.discordapp.com/attachments/420774865369300993/650332109025574935/demon.png",
+                        Title = "Faction Demon",
+                        Description = $"Percentage: {stats.Item2}%"
+                    };
 
-                await ReplyAsync("", false, demon.Build());
+                    await ReplyAsync("", false, demon.Build());
+                }
+                else
+                {
+                    await ReplyAsync("Frozen Crown channel OFFLINE.");
+                }
+            }
+            else
+            {
+                await ReplyAsync("You have to be on #bot-commands.");
             }
         }
     }
