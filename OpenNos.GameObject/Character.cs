2fa57603bfb2eabf44da9e24592db680d5557a5d
Mattia <mattia.grimaldi@arsdigitalia.it>
2020-01-25 12:04:03 +0100

    Revert "restore character title commit"
    This reverts commit b0b2e028397c6b8b040ad3d9a70c3ab337566d25.

--- OpenNos.GameObject/Character.cs
+++ OpenNos.GameObject/Character.cs
@@ -219,6 +219,8 @@ namespace OpenNos.GameObject
 
         public ThreadSafeGenericLockedList<BCard> EquipmentBCards => BattleEntity.BCards;
 
+        public ThreadSafeGenericLockedList<BCard> TitleBCards => BattleEntity.BCards;
+
         public ExchangeInfo ExchangeInfo { get; set; }
 
         public Family Family { get; set; }
@@ -2306,6 +2308,15 @@ namespace OpenNos.GameObject
                     SkillComboCount = 0;
                     Session.SendPackets(GenerateQuicklist());
                     Session.SendPacket("ms_c 1");
+                }
+
+                if (Titles.Where(t => t.Active == true).Any())
+                {
+                    foreach (CharacterTitleDTO titItems in Titles.Where(t => t.Active == true))
+                    {
+                        Item titItem = ServerManager.GetItem(titItems.TitleType);
+                        titItem.BCards.ForEach(s => s.ApplyBCards(Session.Character.BattleEntity, Session.Character.BattleEntity));
+                    }
                 }
                 if (LastPermBuffRefresh.AddSeconds(2) <= DateTime.Now)
                 {
