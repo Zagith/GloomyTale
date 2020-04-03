d25974852dfbcb85b8556f95ac8b39d243a79798
Mattia <mattia.grimaldi@arsdigitalia.it>
2020-04-03 21:39:16 +0200

    update repo

--- Run_Release.bat
+++ Run_Release.bat
@@ -1,55 +1,16 @@
-cd OpenNos.Master.Server
+cd dist/Master
 start OpenNos.Master.Server.exe
-timeout 30
+timeout 10
 
 cd ..
 
-cd OpenNos.World
-
-cd CH1
+cd World
 start OpenNos.World.exe
-timeout 10
+timeout 5000
 
 cd ..
 
-cd CH2
-start OpenNos.World.exe
-timeout 10
-
-cd ..
-
-cd CH3
-start OpenNos.World.exe
-timeout 10
-
-cd ..
-
-cd CH4
-start OpenNos.World.exe
-timeout 10
-
-cd ..
-
-cd CH5
-start OpenNos.World.exe
-timeout 10
-
-cd ..
-
-::cd CH6
-::start OpenNos.World.exe
-::timeout 10
-
-::cd ..
-
-cd CH51
-start OpenNos.World.exe --port 5100
-timeout 10
-
-cd ..
-cd ..
-
-cd OpenNos.Login
+cd Login
 start OpenNos.Login.exe
 
 exit
